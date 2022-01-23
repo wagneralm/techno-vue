c5af7197463fd9f4328961778ebf4159ae930bbe
Wagner de Almeida <wagneer.alm@gmail.com>
2022-01-23 00:51:23 -0300

    Filters para modificar valor da moeda

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const vm = new Vue({
   data: {
     produtos: []
   },
+  filters: {
+    numeroPreco(valor){
+      return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL"});
+    }
+  },
   methods: {
     fecthProdutos(){
       fetch("./api/produtos.json")
