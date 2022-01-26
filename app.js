f99f155758e2fcce3b8252449b9e6e49aad50a65
Wagner de Almeida <wagneer.alm@gmail.com>
2022-01-26 00:38:57 -0300

    Acrescentado router
    Sistema de rotas acrescentado

--- app.js
+++ app.js
@@ -68,15 +68,27 @@ const vm = new Vue({
       setTimeout(() => {
         this.alertaAtivo = false;
       }, 1500);
+    },
+    router() {
+      const hash = document.location.hash;
+      if(hash) {
+        this.fetchProduto(hash.replace("#",""));
+      }
     }
   },
   watch: {
+    produto(){
+      document.title = this.produto.nome || "Techno";
+      const hash = this.produto.id || "";
+      history.pushState(null, null, `#${hash}`);
+    },
     carrinho() {
       window.localStorage.carrinho = JSON.stringify(this.carrinho);
     }
   },
   created() {
     this.fecthProdutos();
+    this.router();
     this.checarLocalStorage();
   }
 })
