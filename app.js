f5f031c4f7e9c27cad9cd9cd3717f2c2861d3d96
Wagner de Almeida <wagneer.alm@gmail.com>
2022-01-25 22:53:34 -0300

    Criação das funcionalidades do carrinho
    Adicionar, remover  e armazenar itens no carrinho.

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const vm = new Vue({
   el: "#app",
   data: {
     produtos: [],
-    produto: false
+    produto: false,
+    carrinho: []
   },
   filters: {
     numeroPreco(valor){
       return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL"});
+    }
+  },
+  computed: {
+    carrinhoTotal() {
+      let total = 0;
+      if(this.carrinho.length) {
+        this.carrinho.forEach(item => {
+          total += item.preco;
+        });
+      }
+      return total;
     }
   },
   methods: {
@@ -30,13 +42,31 @@ const vm = new Vue({
         top: 0,
         behavior: "smooth"
       })
-    }
-    ,
+    },
     fecharModal({target, currentTarget}) {
       if(target === currentTarget) this.produto = false;
+    },
+    adicionarItem() {
+      this.produto.estoque--;
+      const {id, nome, preco} = this.produto;
+      this.carrinho.push({id, nome, preco});
+    },
+    removerItem(index) {
+      this.carrinho.splice(index, 1);
+    },
+    checarLocalStorage() {
+      if(window.localStorage.carrinho) {
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+      }
+    }
+  },
+  watch: {
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
     }
   },
   created() {
     this.fecthProdutos();
+    this.checarLocalStorage();
   }
 })
